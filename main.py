8947437212c45a9b2bc93f93a765ac61450e6291
Jordan <grej8256@student.ashcoll.school.nz>
2021-03-11 20:20:54 +1300

    Test

--- main.py
+++ main.py
@@ -1,105 +1,85 @@
 import random
-
-sad_quotes = ['A million words would not bring you back, I know because I tried, neither would a million tears, '
-              'I know because I cried.',
-              'Things change. And friends leave. Life doesn\'t stop for anybody.',
-              'Breathing is hard. When you cry so much, it makes you realize that breathing is hard.']
-angry_quotes = ['I don\'t usually lose my temper, but if I get angry, it\'s true - I\'m scary.',
-                'Sometimes, you have to get angry to get things done.',
-                'For every minute you remain angry, you give up sixty seconds of peace of mind.']
-tired_quotes = ['Losers quit when they’re tired. Winners quit when they’ve won.',
-                'When I’d get tired and want to stop, I’d wonder what my next opponent was doing. I’d wonder if he '
-                'was still working out. I’d tried to visualize him. When I could see him working, I’d start pushing '
-                'myself. When I could see him in the shower, I’d push myself harder.',
-                'My goal is to build a life that I don’t need a vacation from.']
-happy_quotes = ['Be healthy and take care of yourself, but be happy with the beautiful things that make you, you.',
-                'The biggest adventure you can ever take is to live the life of your dreams.',
-                'Beauty is everywhere. You only have to look to see it.']
-worried_quotes = ['Worry never robs tomorrow of its sorrow, it only saps today of its joy.',
-                  'Worry often gives a small thing a big shadow.',
-                  'People become attached to their burdens sometimes more than the burdens are attached to them.']
-annoyed_quotes = ['I don\'t even want you to nod, that\'s how much you annoy me. Just freeze and shut up.',
-                  'I like long walks, especialy when they are taken by people who annoy me.',
-                  'If you can\'t annoy somebody, there is little point in writing.']
+import tkinter
 
 
 def get_name():
-    """Awaits user input and sets the name value"""
-    name = input("Enter your name: ")
+    # Gets the users name and returns it when called
+    name = input('Enter your name: ')
+    if name == "":
+        print("Please enter your name, you cannot leave it blank!")
+        print()
+        get_name()
 
     return name
 
 
 def welcome_message(name):
-    """This is called at the start and starts loop"""
-    user_name = name
-    print("Welcome to the quote generator, " + user_name)
-    print("")
-    print("The generator will give you a quote corresponding to how you're feeling.")
+    # This runs the welcome message and explains what is happening
+    name_ = name
+    print("Hello, " + name_ + "! \nWelcome to the feelings generator!")
+    print()
+    print("Enter a feeling or the letter corresponding to how you are feeling to generate a quote based off of that "
+          "feeling")
 
 
 def main_loop(name):
-    """This is the main loop where the users answer is handled"""
-
-    """Different feeling types"""
-
+    # This runs the main loop and handles feelings
+    print()
     print('''
     *********************
     *** Feelings Menu ***
     *********************
-    *** A: Sad        ***
-    *** B: Angry      ***
-    *** C: Tired      ***
-    *** D: Happy      ***
-    *** E: Worried    ***
-    *** F: Annoyed    ***
+    ***               ***  
+    ***   A: Happy    ***
+    ***   B: Sad      ***
+    ***   C: Angry    ***
+    ***               ***
+    *********************
+    *** Feelings Menu ***
     *********************
     ''')
 
-    feeling = input("Enter a feeling: ")
+    quotes_sad = ['Things change. And friends leave. Life doesn\'t stop for anybody.',
+                  'Breathing is hard. When you cry so much, it makes you realize that breathing is hard.',
+                  'You cannot protect yourself from sadness without protecting yourself from happiness.']
 
-    if feeling == "sad".lower() or "a".lower():
-        random_number = random.randint(0, len(sad_quotes) - 1)
+    quotes_happy = ['Be healthy and take care of yourself, but be happy with the beautiful things that make you, you.',
+                    'The biggest adventure you can ever take is to live the life of your dreams.',
+                    'Beauty is everywhere. You only have to look to see it.']
 
-        print(sad_quotes[random_number])
-    elif feeling == "angry".lower() or "b".lower():
-        random_number = random.randint(0, len(angry_quotes) - 1)
+    quotes_angry = ['Never go to bed mad. Stay up and fight.',
+                    'Don’t waste your time in anger, regrets, worries, and grudges. Life is too short to be unhappy.',
+                    'The best fighter is never angry.']
 
-        print(angry_quotes[random_number])
-    elif feeling == "tired".lower() or "c".lower():
-        random_number = random.randint(0, len(tired_quotes) - 1)
+    persons_feeling = input('Enter the feeling that\'s closest to how you are feeling: ')
 
-        print(tired_quotes[random_number])
+    # Deals with the persons answers
+    if 'happy' in persons_feeling.lower() or persons_feeling.lower() == 'a':
+        print()
+        print(quotes_happy[random.randint(0, len(quotes_happy) - 1)])
 
-    elif feeling == "happy".lower() or "d".lower():
-        random_number = random.randint(0, len(happy_quotes) - 1)
+    elif 'sad' in persons_feeling.lower() or persons_feeling.lower() == 'b':
+        print()
+        print(quotes_sad[random.randint(0, len(quotes_sad) - 1)])
 
-        print(happy_quotes[random_number])
+    elif 'angry' in persons_feeling.lower() or persons_feeling.lower() == 'c':
+        print()
+        print(quotes_angry[random.randint(0, len(quotes_angry) - 1)])
 
-    elif feeling == "worried".lower() or "e".lower():
-        random_number = random.randint(0, len(worried_quotes) - 1)
+    elif persons_feeling == "":
+        print("Please enter a feeling, you cannot leave it empty!")
+        main_loop(name)
 
-        print(worried_quotes[random_number])
-
-    elif feeling == "annoyed".lower() or "f".lower():
-        random_number = random.randint(0, len(annoyed_quotes) - 1)
-
-        print(annoyed_quotes[random_number])
     else:
-        print("Invalid answer, input one of the feelings above and a quote will follow.")
-        print('Please try again!')
+        print("Sorry, the generator doesn't quite understand that!")
         main_loop(name)
 
 
+def main():
+    # Runs all of the functions
+    name_ = get_name()
+    welcome_message(name_)
+    main_loop(name_)
 
 
-def start():
-    name = get_name()
-    print("")
-    print("")
-    welcome_message(name)
-
-    main_loop(name)
-
-
-start()
+main()
