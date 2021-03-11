15fe8d877dbf40299de494179f96c942b8f0bcb9
GumBux <grej8256@student.ashcoll.school.nz>
2021-03-11 13:16:49 +1300

    Working feelings code, working on quotes later on.

--- FeelingsCode.py
+++ FeelingsCode.py
@@ -4,6 +4,9 @@ import random
 def get_name():
     """Gets the users name and returns it when called"""
     name = input('Enter your name: ')
+    if name == "":
+        print("Please enter your name, you cannot leave it blank!")
+        get_name()
 
     return name
 
@@ -23,9 +26,13 @@ def main_loop(name):
     *********************
     *** Feelings Menu ***
     *********************
-    *** A: Happy      ***
-    *** B: Sad        ***
-    *** C: Angry      ***
+    ***               ***  
+    ***   A: Happy    ***
+    ***   B: Sad      ***
+    ***   C: Angry    ***
+    ***               ***
+    *********************
+    *** Feelings Menu ***
     *********************
     ''')
 
@@ -35,8 +42,18 @@ def main_loop(name):
 
     persons_feeling = input('Enter the feeling that\'s closest to how you are feeling: ')
 
-    if persons_feeling == 'sad'.lower() or 'a'.lower():
+    if 'happy' in persons_feeling.lower() or persons_feeling.lower() == 'a':
+        print(quotes_happy[random.randint(0, len(quotes_happy) - 1)])
+    elif 'sad' in persons_feeling.lower() or persons_feeling.lower() == 'b':
         print(quotes_sad[random.randint(0, len(quotes_sad) - 1)])
+    elif 'angry' in persons_feeling.lower() or persons_feeling.lower() == 'c':
+        print(quotes_angry[random.randint(0, len(quotes_angry) - 1)])
+    elif persons_feeling == "":
+        print("Please enter a feeling, you cannot leave it empty!")
+        main_loop(name)
+    else:
+        print("Sorry, the generator doesn't quite understand that!")
+        main_loop(name)
 
 
 def main():
